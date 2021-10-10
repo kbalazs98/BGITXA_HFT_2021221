cab1df6a78e2c704e3a731d71c24bf7e1d02810d
Koko Yx <karmi.balazs1@gmail.com>
2021-10-10 19:15:57 +0200

    -both relative and absolute path of the connection string added to the  temporary class1 (.Data project)

--- BGITXA_HFT_2021221.Data/Class1.cs
+++ BGITXA_HFT_2021221.Data/Class1.cs
@@ -2,6 +2,8 @@
 
 namespace BGITXA_HFT_2021221.Data
 {
+    //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\karmi\Desktop\BGITXA_HFT_2021221\BGITXA_HFT_2021221\BGITXA_HFT_2021221.Data\Database.mdf;Integrated Security=True
+    //Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename="|DataDirectory|\Database.mdf;Integrated Security=True
     public class Class1
     {
     }
