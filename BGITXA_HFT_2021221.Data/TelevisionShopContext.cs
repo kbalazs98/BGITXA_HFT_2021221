f65ee9c7c4d291d72dff19b517a5d5a7499658b3
Koko Yx <karmi.balazs1@gmail.com>
2021-10-17 15:48:46 +0200

    Empty Classes created in Models project.
    Empty Context class created

---
+++ BGITXA_HFT_2021221.Data/TelevisionShopContext.cs
@@ -0,0 +1,17 @@
+﻿using BGITXA_HFT_2021221.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BGITXA_HFT_2021221.Data
+{
+    class TelevisionShopContext : DbContext
+    {
+        public virtual DbSet<Television> Televisions { get; set; }
+        public virtual DbSet<Brand> Brands { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+    }
+}
