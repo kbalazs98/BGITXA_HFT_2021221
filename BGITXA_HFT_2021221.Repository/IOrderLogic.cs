abcfe557c9e552193c18335c016f699baff607d9
Koko Yx <karmi.balazs1@gmail.com>
2021-11-13 17:05:51 +0100

    IOrderLogic and ITelevisionRepository interfaces are also finished.

--- BGITXA_HFT_2021221.Repository/IOrderLogic.cs
+++ BGITXA_HFT_2021221.Repository/IOrderLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BGITXA_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace BGITXA_HFT_2021221.Repository
 {
     public interface IOrderLogic
     {
+        //CRUD
+        void Create(Order order);
+        void ReadOne(int id);
+        IQueryable<Order> ReadAll();
+        void Update(Order order);
+        void Delete(int orderId);
     }
 }
--- BGITXA_HFT_2021221.Repository/ITelevisionRepository.cs
+++ BGITXA_HFT_2021221.Repository/ITelevisionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using BGITXA_HFT_2021221.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,11 @@ namespace BGITXA_HFT_2021221.Repository
 {
     public interface ITelevisionRepository
     {
+        //CRUD
+        void Create(Television television);
+        void ReadOne(int id);
+        IQueryable<Television> ReadAll();
+        void Update(Television television);
+        void Delete(int televisionId);
     }
 }
